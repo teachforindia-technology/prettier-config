06fc8e3d314efc4094c5a089cd1430211824c300
eels <gauraang.bhartiya@teachforindia.org>
2019-10-20 23:29:18 +0530

    Pretty

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # prettier-config
+
 Prettier config for all projects
+
+# Usage
+
+## Install
+
+```
+npm install --dev tfi-prettier-config
+```
+
+## Edit `package.json`
+
+```jsonc
+{
+	// ...
+	"prettier": "tfi-prettier-config"
+}
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: "es5",
+	bracketSpacing: false,
+	jsxBracketSameLine: false,
+	arrowParens: "avoid",
+	requirePragma: false
+}
